991fcd4312c6248b718e61caa8066b2c4f4a04fc
Brian H Wilson <bwilson@co.clatsop.or.us>
2022-01-25 09:29:49 -0800

    added required baby turtle picture

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -1,5 +1,11 @@
+<script>
+    const imageUrl = new URL('../assets/babyturtle.png?as=jpeg&quality=25&width=300',import.meta.url
+    )
+</script>
+
 <div class="About">
-    <img alt="Baby turtle" src="../assets/babyturtle.png">
+    <img alt="Baby turtle" src="{imageUrl}">
+
     <p>
         This template is for building apps based on the <a href="https://svelte.dev">Svelte framework</a> 
         and the <a href="https://parceljs.org">Parcel</a> build tool.
