39a5a30875225446950693691b8d38bf6c1a5608
Brian H Wilson <bwilson@co.clatsop.or.us>
2022-02-17 12:29:39 -0800

    cleaned up login form

--- src/pages/Login.svelte
+++ src/pages/Login.svelte
@@ -3,28 +3,57 @@
 </script>
 
 <style>
-    .accordion{
-        background-color: aquamarine;
+    * {
+        box-sizing: border-box;
+    }
+    h2 {
+        padding-left: 0.4em;
+    }
+    .svelte-login {
+        display: flex;
+        flex-direction: column;
+        width: 25em;
+        background-color: rgb(219, 223, 231);
+        box-shadow: 0.1rem 0.1rem 2.8rem 0.01rem;
+        padding: 0px 5px 7px 5px;
+        margin: 0 auto 0 auto;
+    }
+
+    fieldset {
+        box-sizing: content-box;
+        overflow: hidden;
+        padding: 2px;
+        border: none;
+    }
+
+    input {
+        width: 100%;
+    }
+
+    #signin-button {
+        width:100%;
+        color: #eee;
+        background-color: #524bbb;
+        -webkit-padding: 0.4em 0;
+        padding: 0.4em;
+        outline: none;
+        border: none;
     }
 </style>
 
-<section class="svelte-login">
-<div class="accordion">
-    <div id="login_form">
-        <h2>Sign in</h2>
-        <form>
+<div class="svelte-login">
+    <h2>Sign in</h2>
+    <div class="alert-message" id="error-message"></div>
+    <form>
+        <fieldset>
             <input type="hidden" id="oauth_state" name="oauth_state" value="">
             <input type="hidden" id="authorize" name="authorize" value="true">
-            <input required id="username" type="text" name="username" placeholder="Username"><br/>
-            <input required id="password" type="password" name="password" placeholder="Password"><br />
-            <div class="flex">
-            <button id="signin" name="signin">Sign In</button>
-            </div>
-        </form>
-    </div>
-
+            <input required type="text" name="username" placeholder="Username"><br/>
+            <input required type="password" name="password" placeholder="Password"><br />
+            <button type="submit" id="signin-button" name="signin">Sign In</button>
+        </fieldset>
+    </form>
     <div>
         <a href="forgotpassword">Forgot password?</a>
     </div>
 </div>
-</section>
